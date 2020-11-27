a097fb9ea6125dabd08764f40a336a08f772c841
Erick Bernardo <erickvictor@gmail.com>
2020-11-26 21:09:32 -0400

    DataTable Filter and Search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Datatable from "./datatable";
+
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
 function App() {
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState("");
+  const [searchColumns, setSearchColumns] = useState(["firstName", "lastName"]);
+
+  useEffect(() => {
+    fetch("https://devmentor.live/api/examples/contacts?api_key=b7c58b")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  function search(rows) {
+    return rows.filter((row) =>
+      columns.some(
+        (column) =>
+          row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
+      )
+    );
+  }
+
+  const columns = data[0] && Object.keys(data[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+        {columns &&
+          columns.map((column) => (
+            <label>
+              <input
+                type="checkbox"
+                checked={searchColumns.includes(column)}
+                onChange={(e) => {
+                  const checked = searchColumns.includes(column);
+                  setSearchColumns((prev) =>
+                    checked
+                      ? prev.filter((sc) => sc !== column)
+                      : [...prev, column]
+                  );
+                }}
+              />
+              {column}
+            </label>
+          ))}
+      </div>
+      <div>
+        <Datatable data={search(data)} />
+      </div>
     </div>
   );
 }
